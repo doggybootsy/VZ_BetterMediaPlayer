06e7f2f445ab94adf43e5e3aacb496b56f818a5e
doggybootsy <71196819+doggybootsy@users.noreply.github.com>
2021-09-25 18:50:01 -0700

    clean up

--- index.js
+++ index.js
@@ -20,11 +20,14 @@ module.exports = class BetterMediaPlayer extends Plugin {
 	}
 	PIP(node) {
 		try {
-			if(document.pictureInPictureElement) document.exitPictureInPicture()
-			else node.parentNode.previousSibling.requestPictureInPicture()
+			if(document.pictureInPictureElement) 
+				document.exitPictureInPicture()
+			else 
+				node.parentNode.previousSibling.requestPictureInPicture()
 			node.parentNode.previousSibling.addEventListener("leavepictureinpicture", leavepip)
 			const leavepip = () => node.parentNode.previousSibling.removeEventListener("leavepictureinpicture", leavepip)
-		} catch(e){
+		} 
+		catch(e){
 			this.error(e)
 		}
 	}
@@ -32,18 +35,22 @@ module.exports = class BetterMediaPlayer extends Plugin {
 		try {
 			const ele = node.parentNode.previousSibling
 			ele.loop = !ele.loop
-		} catch (e) {
+		} 
+		catch (e) {
 			this.error(e)
 		}
 	}
 	start() {
 		const { get } = this.settings
 		patch("BetterMediaPlayer-Contols", Contols.prototype, "render", (_, res) => {
-			if(get("button_pip", true) === true) res.props.children.splice(get("position_pip", 1), 0, React.createElement(PipIcon, {instance: this}))
-			if(get("button_loop", true) === true) res.props.children.splice(get("position_loop", 1), 0, React.createElement(LoopIcon, {instance: this, active: get("auto_loop", true)}))
+			if(get("button_pip", true) === true) 
+				res.props.children.splice(get("position_pip", 1), 0, React.createElement(PipIcon, {instance: this}))
+			if(get("button_loop", true) === true) 
+				res.props.children.splice(get("position_loop", 1), 0, React.createElement(LoopIcon, {instance: this, active: get("auto_loop", true)}))
 		})
 		patch("BetterMediaPlayer-AutoLoop", MediaPlayer.prototype, "renderVideo", (_, res) => {
-			if(get("auto_loop", true)) res.props.loop = true
+			if(get("auto_loop", true)) 
+				res.props.loop = true
 		})
 	}
 	stop() {}
