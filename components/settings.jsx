8965abb6d5c8aa74eb7a2a257fb809047b392aa1
doggybootsy <71196819+doggybootsy@users.noreply.github.com>
2021-09-26 17:27:03 -0700

    l

--- components/settings.jsx
+++ components/settings.jsx
@@ -23,13 +23,13 @@ module.exports = class Settings extends React.PureComponent {
 		const { getSetting, updateSetting, toggleSetting } = this.props
 		return (
             <>
-                <Category opened onClick={(e) => this.Cat_onClick(e, "cat_demo")} title="Demo" description="little preview" icon={
+                <Category opened={true} onClick={(e) => this.Cat_onClick(e, "cat_demo")} title="Demo" description="little preview" icon={
                     <Icon name={this.state.cat_demo === true ? "Eye" : "EyeHidden"} width="2rem" height="2rem" style={{padding: ".25rem"}}/>
                 }><FakeMessage /></Category>
                 <Category onClick={(e) => this.Cat_onClick(e, "cat_loop")} title="Loop" description="Loop configuration" icon={
 			        <svg width="1.75rem" height="1.75rem" style={{padding: ".5rem"}} viewBox="-5 0 459 459.648" xmlns="http://www.w3.org/2000/svg">
-				        <path fill={this.state.cat_loop === true ? "var(--brand-experiment)" : "currentColor"} d="m416.324219 293.824219c0 26.507812-21.492188 48-48 48h-313.375l63.199219-63.199219-22.625-22.625-90.511719 90.511719c-6.246094 6.25-6.246094 16.375 0 22.625l90.511719 90.511719 22.625-22.625-63.199219-63.199219h313.375c44.160156-.054688 79.945312-35.839844 80-80v-64h-32zm0 0"></path>
-				        <path fill={this.state.cat_loop === true ? "var(--brand-experiment)" : "currentColor"} d="m32.324219 165.824219c0-26.511719 21.488281-48 48-48h313.375l-63.199219 63.199219 22.625 22.625 90.511719-90.511719c6.246093-6.25 6.246093-16.375 0-22.625l-90.511719-90.511719-22.625 22.625 63.199219 63.199219h-313.375c-44.160157.050781-79.949219 35.839843-80 80v64h32zm0 0"></path>
+				        <path style={{transition: "fill 0.2s"}} fill={this.state.cat_loop === true ? "var(--brand-experiment)" : "currentColor"} d="m416.324219 293.824219c0 26.507812-21.492188 48-48 48h-313.375l63.199219-63.199219-22.625-22.625-90.511719 90.511719c-6.246094 6.25-6.246094 16.375 0 22.625l90.511719 90.511719 22.625-22.625-63.199219-63.199219h313.375c44.160156-.054688 79.945312-35.839844 80-80v-64h-32zm0 0"></path>
+				        <path style={{transition: "fill 0.2s"}} fill={this.state.cat_loop === true ? "var(--brand-experiment)" : "currentColor"} d="m32.324219 165.824219c0-26.511719 21.488281-48 48-48h313.375l-63.199219 63.199219 22.625 22.625 90.511719-90.511719c6.246093-6.25 6.246093-16.375 0-22.625l-90.511719-90.511719-22.625 22.625 63.199219 63.199219h-313.375c-44.160157.050781-79.949219 35.839843-80 80v64h32zm0 0"></path>
 			        </svg>
                 }>
                     <SwitchItem 
@@ -59,8 +59,8 @@ module.exports = class Settings extends React.PureComponent {
                 </Category>
                 <Category onClick={(e) => this.Cat_onClick(e, "cat_pip")} title="Picture in picture" description="Picture in picture configuration" icon={
 			        <svg width="1.75rem" height="2rem" style={{padding: ".5rem"}} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-			        	<path fill="transparent" d="M0 0h24v24H0V0z"></path>
-			        	<path fill={this.state.cat_pip === true ? "var(--brand-experiment)" : "currentColor"} d="M19 11h-8v6h8v-6zm4 8V4.98C23 3.88 22.1 3 21 3H3c-1.1 0-2 .88-2 1.98V19c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2zm-2 .02H3V4.97h18v14.05z"></path>
+			        	<path style={{transition: "fill 0.2s"}} fill="transparent" d="M0 0h24v24H0V0z"></path>
+			        	<path style={{transition: "fill 0.2s"}} fill={this.state.cat_pip === true ? "var(--brand-experiment)" : "currentColor"} d="M19 11h-8v6h8v-6zm4 8V4.98C23 3.88 22.1 3 21 3H3c-1.1 0-2 .88-2 1.98V19c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2zm-2 .02H3V4.97h18v14.05z"></path>
 			        </svg>
                 }>
                     <SwitchItem 
